54c8696af2422044ad6a562aeed5e6ed1c574efc
K.Amrutha Lakshmi <148676901+Amrutha57@users.noreply.github.com>
2025-02-04 23:33:05 +0530

    started
    started

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Moviecard from './components/moviecards';
+function App() {
+  return (
+    <>
+   <Moviecard  movie={{title:"Amrutha" ,release:"12/3/25"}}/>
+    </>
+  );
+}
+export default App
---
+++ vite-project/src/components/moviecards.jsx
@@ -0,0 +1,20 @@
+function Moviecard({movie}){
+    function onclicked(){
+        alert('clicked')
+    }
+    return  <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onclicked}>
+                🤍
+                </button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release}</p>
+        </div>
+    </div>
+}
+export default Moviecard
